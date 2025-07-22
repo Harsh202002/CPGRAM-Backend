f9d5371afc3a09490de2c6f5d50ce22823c67a10
mohitkumar0831 <mohitkumar@netfotech.in>
2025-07-22 12:48:21 +0530

    updated grievance controller and route

--- routes/grievanceRoutes.js
+++ routes/grievanceRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createGrievance, getUserGrievances, getGrievancesByUniqueId, updateGrievanceStatus, updateGrievance,addProgressUpdate, trackGrievance, deleteProgressUpdate,deleteLastActivityLog} = require('../controllers/grievanceController');
+const { createGrievance, getUserGrievances, getGrievancesByUniqueId, updateGrievanceStatus, updateGrievance,addProgressUpdate, trackGrievance, deleteProgressUpdate,deleteLastActivityLog, getGrievancesAssignedToOfficer} = require('../controllers/grievanceController');
 const { protect } = require('../middlewares/authMiddleware');
 const { upload } = require('../middlewares/uploadMiddleware');
 // const {body} = require('express-validator');
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post('/create', protect, upload.array('attachments', 5), createGrievance);
 router.get('/my-grievances', protect, getUserGrievances);
 router.get('/grievance/:uniqueID', protect, getGrievancesByUniqueId);
+router.get("/assigned", protect, getGrievancesAssignedToOfficer);
 router.put('/status/:grievanceId', protect, updateGrievanceStatus);
 router.put('/update/:grievanceId', protect, upload.array('attachments', 5), updateGrievance);
 router.post('/progress/:grievanceId',protect,addProgressUpdate)
