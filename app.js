903935fb4c74fdf959b981f7499d8442d9988970
Harsh202002 <harshit@netfotech.in>
2025-08-06 15:22:04 +0530

    some changes made

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "https://cpgrams-frontend.vercel.app", // Allow all origins by default
+    origin: "*", // Allow all origins by default
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], //
     credentials: true,
   })
